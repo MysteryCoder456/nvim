cdf2c5c438b6dd27dea1bbb6401b553e82b0c874
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-20 23:08:08 +0400

    ehhh

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,21 +18,29 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 })
 
 lsp.set_preferences({
-    sign_icons = { }
+    sign_icons = {}
 })
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+--lsp.on_attach(function(client, bufnr)
+--    local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-end)
+--    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+--    vim.keymap.set("n", "F", function() vim.lsp.buf.format() end, opts)
+--    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+--    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+--end)
+
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+vim.keymap.set("n", "F", function() vim.lsp.buf.format() end, opts)
+vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 
 lsp.setup()
