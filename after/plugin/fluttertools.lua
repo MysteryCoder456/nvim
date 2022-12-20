0038b43046702282d4910dabfb01cd4bc0365516
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-20 21:22:13 +0400

    updated flutter config

--- after/plugin/fluttertools.lua
+++ after/plugin/fluttertools.lua
@@ -1,1 +1,13 @@
-require("flutter-tools").setup{}
+local flutter = require("flutter-tools")
+
+flutter.setup({
+	ui = {
+		border = "rounded",
+	},
+	decorations = {
+		statusline = {
+			app_version = false,
+			device = false,
+		}
+	},
+})
