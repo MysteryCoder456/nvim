dfa710e56d13024a14446b4661c144471ec06b1c
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-22 00:57:48 +0400

    removed colorcolumn and made line numbers transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,11 +2,13 @@ function ColorMyPencils(color, airline)
     color = color or "oxocarbon"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
     airline = airline or "night_owl"
     vim.cmd.AirlineTheme(airline)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.highlight("clear", "LineNr")
+    vim.cmd.highlight("clear", "SignColumn")
 end
 
 ColorMyPencils()
