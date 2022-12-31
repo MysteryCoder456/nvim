e1b812ee09c7767ba417a14a6de2e567b9f1bf75
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-31 12:24:25 +0400

    added prettier formatter

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -4,5 +4,6 @@ null_ls.setup {
     sources = {
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.formatting.black.with({ extra_args = { "-l", "79" } }),
+        null_ls.builtins.formatting.prettier,
     }
 }
