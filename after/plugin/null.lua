e183078893018ab311a65dc0129cf961a66e6d3f
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-01-09 19:00:51 +0400

    replaced flake8 with ruff

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -2,7 +2,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup {
     sources = {
-        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.formatting.black.with({ extra_args = { "-l", "79" } }),
         null_ls.builtins.formatting.prettier,
     }
