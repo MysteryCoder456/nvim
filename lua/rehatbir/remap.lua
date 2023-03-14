782e6fd6673551411a5d25dadb4413a0a0cd8771
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-03-14 13:44:54 +0400

    added remaps for pane switching

--- lua/rehatbir/remap.lua
+++ lua/rehatbir/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Pane switching
+vim.keymap.set("n", "<leader>a", "<C-w>h")
+vim.keymap.set("n", "<leader>s", "<C-w>j")
+vim.keymap.set("n", "<leader>w", "<C-w>k")
+vim.keymap.set("n", "<leader>d", "<C-w>l")
