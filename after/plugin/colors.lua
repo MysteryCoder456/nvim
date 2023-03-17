96a24b641c6944afde7c2ef05621059b46b1fb49
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-03-17 21:38:07 +0400

    reenabled transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,10 +5,10 @@ function ColorMyPencils(color, airline)
     airline = airline or "base16_outrun_dark"
     vim.cmd.AirlineTheme(airline)
 
-    --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    --vim.cmd.highlight("clear", "LineNr")
-    --vim.cmd.highlight("clear", "SignColumn")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.highlight("clear", "LineNr")
+    vim.cmd.highlight("clear", "SignColumn")
 end
 
 ColorMyPencils()
