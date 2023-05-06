c5eaf2ac595b6f0b7ab4f3745040a4e447539339
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-05-06 20:14:12 +0400

    added gdformat and gdlint

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -5,5 +5,7 @@ null_ls.setup {
         null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.formatting.black.with({ extra_args = { "-l", "79" } }),
         null_ls.builtins.formatting.prettier.with({ disabled_filetypes = { "html" } }),
+        null_ls.builtins.formatting.gdformat,
+        null_ls.builtins.diagnostics.gdlint,
     }
 }
