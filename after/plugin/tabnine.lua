2bd1125a55684fad234c012a6da27cff7f9bf790
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-05-10 18:40:14 +0400

    disabled tabnine plugin

--- after/plugin/tabnine.lua
+++ after/plugin/tabnine.lua
@@ -1,5 +1,5 @@
-require("tabnine").setup {
-    accept_keymap = "<C-t>",
-    suggestion_color = { gui = "#B08F4E" },
-    execlude_filetypes = { "TelescopePrompt" }
-}
+-- require("tabnine").setup {
+--     accept_keymap = "<C-t>",
+--     suggestion_color = { gui = "#B08F4E" },
+--     execlude_filetypes = { "TelescopePrompt" }
+-- }
