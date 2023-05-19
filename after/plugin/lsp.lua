c667c488afd0ed663e028d697faf2b94049f6475
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-05-19 23:55:21 +0400

    changed prettier tab width to 4 spaces

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -55,7 +55,7 @@ null_ls.setup {
     sources = {
         null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.formatting.black.with({ extra_args = { "-l", "79" } }),
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({ extra_args = { "--tab-width", "4" } }),
         null_ls.builtins.formatting.gdformat,
         null_ls.builtins.diagnostics.gdlint,
     }
