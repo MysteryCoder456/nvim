3e54896ee708f2474ef12b8f9299fb3bd834a51a
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-09-07 21:53:56 +0400

    added colorizer plugin

--- lua/rehatbir/packer.lua
+++ lua/rehatbir/packer.lua
@@ -1,8 +1,8 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use "wbthomason/packer.nvim"
 
 
     -- ==================== THEMES ====================
@@ -23,8 +23,8 @@ return require('packer').startup(function(use)
     use { "savq/melange-nvim", as = "melange", }
 
     use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons' }
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons" }
     }
 
 
@@ -32,51 +32,52 @@ return require('packer').startup(function(use)
 
     use "tpope/vim-surround"
     use "jiangmiao/auto-pairs"
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'nvim-treesitter/playground'
+    use "lukas-reineke/indent-blankline.nvim"
+    use "nvim-treesitter/playground"
     use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }
-    use { 'ThePrimeagen/harpoon', requires = 'nvim-lua/plenary.nvim' }
+    use { "ThePrimeagen/harpoon", requires = "nvim-lua/plenary.nvim" }
 
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
     use {
-        'nvim-treesitter/nvim-treesitter-context',
-        requires = { 'nvim-treesitter/nvim-treesitter' }
+        "nvim-treesitter/nvim-treesitter-context",
+        requires = { "nvim-treesitter/nvim-treesitter" }
     }
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        "nvim-telescope/telescope.nvim", tag = "0.1.0",
+        -- or                            , branch = "0.1.x",
+        requires = { { "nvim-lua/plenary.nvim" } }
     }
 
-    use 'mbbill/undotree'
+    use "mbbill/undotree"
+    use "norcalli/nvim-colorizer.lua"
 
 
     -- ===================== LSP ======================
 
     use {
-        'VonHeikemen/lsp-zero.nvim',
+        "VonHeikemen/lsp-zero.nvim",
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
         }
     }
 
-    use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' }
+    use { "akinsho/flutter-tools.nvim", requires = "nvim-lua/plenary.nvim" }
 
     use {
         "folke/trouble.nvim",
@@ -91,15 +92,15 @@ return require('packer').startup(function(use)
         requires = { "nvim-lua/plenary.nvim" },
     }
 
-    -- use { 'codota/tabnine-nvim', run = "./dl_binaries.sh" }
+    -- use { "codota/tabnine-nvim", run = "./dl_binaries.sh" }
 
     use "ray-x/lsp_signature.nvim"
 
 
     -- ==================== OTHERS ====================
 
-    use 'andweeb/presence.nvim'
-    use 'airblade/vim-gitgutter'
+    use "andweeb/presence.nvim"
+    use "airblade/vim-gitgutter"
 
     use({
         "utilyre/barbecue.nvim",
