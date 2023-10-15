cc6a41dfb669f134be3e0cae0d7d9d0bf1c9e133
Rehatbir Singh <rehatbir.singh@gmail.com>
2023-10-15 19:54:21 +0400

    aa

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,14 +1,14 @@
+-- Initialize mason
+require("mason").setup()
+
 local lsp = require('lsp-zero')
 local lspconfig = require("lspconfig")
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-    "pyright",
-    "rust_analyzer",
-    "emmet_ls",
-    "ruff_lsp",
-})
+lspconfig.pyright.setup {}
+lspconfig.rust_analyzer.setup {}
+lspconfig.ruff_lsp.setup {}
 lspconfig.sourcekit.setup {}
 lspconfig.rome.setup {
     cmd = { "--indent-style", "space", "--indent-size", "4" }
@@ -17,19 +17,21 @@ lspconfig.gdscript.setup {}
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
 
 lsp.set_preferences({
     sign_icons = {}
 })
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    })
 })
 
 --lsp.on_attach(function(client, bufnr)
