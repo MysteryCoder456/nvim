8c38f6d41a80eb2271343bc7875e51d655c34164
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-05-15 17:07:16 +0400

    bootstrap packer to automatically install it

--- lua/rehatbir/packer.lua
+++ lua/rehatbir/packer.lua
@@ -1,3 +1,17 @@
+-- Install packer if not installed
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
@@ -132,4 +146,9 @@ return require("packer").startup(function(use)
         "stevearc/oil.nvim",
         requires = { "nvim-tree/nvim-web-devicons" }
     }
+
+    -- Sync packer packages
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
