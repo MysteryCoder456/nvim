fb60a657fb4b639781c86269328c9ab38f620ff2
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-05-26 09:44:37 +0400

    added djlint for formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -65,8 +65,9 @@ null_ls.setup {
         null_ls.builtins.diagnostics.gdlint,
         null_ls.builtins.diagnostics.swiftlint,
         null_ls.builtins.formatting.black.with({ extra_args = { "-l", "79" } }),
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.djlint,
         null_ls.builtins.formatting.gdformat,
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.swiftformat,
     }
 }
