4425d3eb45d83b4f56916169fa96cbc1f29d618e
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-06-04 23:48:13 +0400

    moved harpoon remaps

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -4,6 +4,18 @@ vim.keymap.set("n", "<leader>-", vim.cmd.Oil)
 -- Undotree
 vim.keymap.set("n", "<leader>u", require("undotree").toggle)
 
+-- Harpoon
+local harpoon = require("harpoon")
+
+vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+
 -- Pane switching
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
--- plugin/harpoon.lua
+++ plugin/harpoon.lua
@@ -3,16 +3,3 @@ local harpoon = require("harpoon")
 -- REQUIRED
 harpoon:setup()
 -- REQUIRED
-
-vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
-
--- Toggle previous & next buffers stored within Harpoon list
--- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
--- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
