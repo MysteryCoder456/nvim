cf03f624bd6bfac5973d047372074a9c461fdf1a
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-06-05 00:21:17 +0400

    nvim-cmp

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,5 +1,6 @@
-require("mason").setup {}
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+require("mason").setup {}
 require("mason-lspconfig").setup {
     ensure_installed = {
         "lua_ls",
@@ -9,16 +10,17 @@ require("mason-lspconfig").setup {
         "clangd",
         "cmake",
         "glsl_analyzer",
-        "emmet_ls",
-        "tailwindcss",
+        "emmet_ls", "tailwindcss",
     },
 }
 require("mason-lspconfig").setup_handlers {
     -- The first entry (without a key) will be the default handler
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {
+            capabilities = cmp_capabilities,
+        }
     end,
     -- Next, you can provide a dedicated handler for specific servers.
     -- For example, a handler override for the `rust_analyzer`:
@@ -26,5 +28,52 @@ require("mason-lspconfig").setup_handlers {
     --     require("rust-tools").setup {}
     -- end
 }
+-- require("lspconfig")
 
-require("lspconfig")
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+cmp.setup {
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+            return kind
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<CR>"] = cmp.mapping({
+            i = function(fallback)
+                if cmp.visible() and cmp.get_active_entry() then
+                    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                else
+                    fallback()
+                end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    })
+}
+
+-- Virtual text errors
+vim.diagnostic.config {
+    virtual_text = true,
+}
+
+-- Format on save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
