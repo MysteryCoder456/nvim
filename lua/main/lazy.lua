369ad93f687c915f565333acd25395d989e6c1d0
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-09-06 14:52:48 -0500

    added markdown preview

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -80,6 +80,12 @@ require("lazy").setup {
     {
         "lewis6991/gitsigns.nvim",
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
 
     -- LSP Related Plugins --
     {
