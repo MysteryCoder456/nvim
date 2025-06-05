5d3281fca61cf48a5c3522b28f521456486c78e4
MysteryCoder456 <rehatbir.singh@gmail.com>
2025-06-05 13:26:15 +0400

    feat: enabled copilot inline auto trigger

--- plugin/copilot.lua
+++ plugin/copilot.lua
@@ -16,7 +16,7 @@ require("copilot").setup {
     },
     suggestion = {
         enabled = true,
-        auto_trigger = false,
+        auto_trigger = true,
         hide_during_completion = true,
         debounce = 75,
         trigger_on_accept = true,
