58e2fcc55d43da61176288fe52483e61fb5e37aa
MysteryCoder456 <rehatbir.singh@gmail.com>
2025-07-17 18:13:55 +0400

    removed prettier (in favor of biome)

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -100,7 +100,6 @@ null_ls.setup {
     sources = {
         null_ls.builtins.formatting.black.with({ extra_args = { "-l", "79" } }),
         null_ls.builtins.formatting.djlint.with({ filetypes = { "html", "django", "htmldjango" } }),
-        null_ls.builtins.formatting.prettierd.with({ filetypes = { "jsonc", "json", "typescript", "astro", "javascriptreact", "javascript", "typescriptreact", "scss", "vue", "handlebars", "graphql", "svelte", "markdown", "css", "yaml", "less", "markdown.mdx" } }), -- All supported files except html
         null_ls.builtins.diagnostics.checkmake,
         null_ls.builtins.diagnostics.djlint.with({ filetypes = { "html", "django", "htmldjango" } }),
     },
