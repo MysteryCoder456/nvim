ddd6683dc6ac3ff2bbf9eba98037e2040aefc283
Rehatbir Singh <rehatbir.singh@gmail.com>
2026-04-27 12:16:50 -0500

    fix: lspkind setup

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -66,6 +66,7 @@ require("mason-lspconfig").setup {
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local lspkind = require("lspkind")
 cmp.setup {
     sources = {
         { name = "nvim_lsp" },
@@ -76,15 +77,24 @@ cmp.setup {
         documentation = cmp.config.window.bordered({ border = 'rounded' }),
     },
     formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "    (" .. (strings[2] or "") .. ")"
+        fields = { "icon", "abbr", "kind", "menu" },
+        format = lspkind.cmp_format({
+            maxwidth = {
+                -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                -- can also be a function to dynamically calculate max width such as
+                -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+                menu = 50,            -- leading text (labelDetails)
+                abbr = 50,            -- actual suggestion item
+            },
+            ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-            return kind
-        end,
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+                return vim_item
+            end
+        }),
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
